4b30a285c08b08591ff0577ed50228bdaa90fb56
Numan Safi <numansafi@live.com>
2021-10-29 06:46:04 +0100

    implemented a delete function within view

--- projects/urls.py
+++ projects/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
 
     path('update-project/<str:pk>', views.updateProject, name='update-project'),
 
+    path('delete-project/<str:pk>', views.deleteProject, name='delete-project'),
+
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -41,3 +41,13 @@ def updateProject(request,pk):
             return redirect('projects')
     context = {'form':form}
     return render(request, "projects/project_form.html", context)
+
+def deleteProject(request,pk):
+    project = Project.objects.get(id=pk)
+    if request.method =="POST":
+        # Delete project from database
+        project.delete()
+        # redirect to 'projects' page
+        return redirect('projects')
+    context = {'object':project}
+    return render(request, 'projects/delete_template.html',context)
